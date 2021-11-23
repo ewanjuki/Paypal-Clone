fbb57518ca3d418a51f2e900e4d5353a8c02fa2a
Edwin Wanjuki <edwinwanjuki@gmail.com>
2021-11-23 18:28:47 +0300

    create & style login page

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const dropdownItems = document.querySelectorAll('.dropdown-hover');
 dropdownItems.forEach(dropdownItem => {
     dropdownItem.addEventListener('mouseover', () => {
         dropdownItem.lastElementChild.style.cssText = 
-            `opacity:1; visibility: visible`;
+            `opacity:1; visibility: visible;`;
         document.querySelector('.navbar-wrapper').style.background = 
             "linear-gradient(to right, #066399, #2f8fdf, #066399)";
     });
 
     dropdownItem.addEventListener('mouseout', () => {
         dropdownItem.lastElementChild.style.cssText = 
-            `opacity:0; visibility: hidden`;
+            `opacity:0; visibility: hidden;`;
         document.querySelector('.navbar-wrapper').style.background = "none";        
     });
 });  
